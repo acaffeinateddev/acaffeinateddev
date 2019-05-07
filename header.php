2c05861bc60944511517a8bef9ef9e3372479f90
Leila <hello@acaffeinated.dev>
2019-05-07 06:37:35 -0400

    Added font awesome support
    Added font awesome support and social media icons.

--- header.php
+++ header.php
@@ -19,6 +19,9 @@
 
 	<?php wp_head(); ?>
     <link href="https://fonts.googleapis.com/css?family=Muli:200,700,900" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+
 </head>
 
 <body <?php body_class(); ?>>
--- sidebar.php
+++ sidebar.php
@@ -16,6 +16,11 @@
                     <img src="https://acaffeinated.dev/wp-content/themes/acaffeinateddev/acd_logo_small.png" alt="A Caffeinated Dev" />
                 </a>
             </div>
+			<div class="social-media">
+				<a class="fab fa-github" href="https://github.com/acaffeinateddev" target="_blank"></a>
+				<a class="fab fa-twitter" href="https://twitter.com/acaffeinateddev" target="_blank"></a>
+				<a class="fab fa-linkedin-in" href="https://www.linkedin.com/in/leilambaker/" target="_blank"></a>
+			</div>
             <nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Navigation', 'acaffeinateddev' ); ?></button>
 			<?php
